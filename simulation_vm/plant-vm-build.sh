c46873cdf2d17094604f1d65eb34f896b0ef0630
Valiant-Mouse <31675444+Valiant-Mouse@users.noreply.github.com>
2018-10-18 17:39:30 -0400

    added -y to apt-get upgrade

--- simulation_vm/plant-vm-build.sh
+++ simulation_vm/plant-vm-build.sh
@@ -4,7 +4,7 @@ echo 52.247.160.149 git.cybbh.space >> /etc/hosts
 sed -i's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 export DEBIAN_FRONTEND=noninteractive\n\
 apt-get update
-apt-get upgrade
+apt-get -y upgrade
 apt-get -y install locate dnsutils lsof freerdp
 apt-get -y install vim-gtk subversion wine build-essential pkg-config bison flex autoconf automake libtool make nodejs git
 svn export https://github.com/djformby/GRFICS/trunk/simulation_vm
