25e7d16727bdb6873b98401a7d9f9e03776bc90c
Valiant-Mouse <31675444+Valiant-Mouse@users.noreply.github.com>
2018-10-18 17:30:00 -0400

    a script to further automate the build

---
+++ simulation_vm/plant-vm-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo 127.0.0.1 $(hostname) >> /etc/hosts
+echo 52.247.160.149 git.cybbh.space >> /etc/hosts
+sed -i's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+export DEBIAN_FRONTEND=noninteractive\n\
+apt-get update
+apt-get upgrade
+apt-get -y install locate dnsutils lsof freerdp
+apt-get -y install vim-gtk subversion wine build-essential pkg-config bison flex autoconf automake libtool make nodejs git
+svn export https://github.com/djformby/GRFICS/trunk/simulation_vm
+mv simulation_vm /home/user
+cd /home/user/simulation_vm/HMI_Simulation_Ubuntu1604_15_x86_64
+bash ./build.sh
+useradd $user -m -U -s /bin/bash
+usermod -aG sudo $user
+echo "root:$password" | chpasswd
+echo "$user:$password" | chpasswd
+reboot
