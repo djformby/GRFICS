ca1385a3b016aa99a5c4cff6736e2500cc601516
Valiant-Mouse <31675444+Valiant-Mouse@users.noreply.github.com>
2018-10-18 14:05:21 -0400

    change the plc_vm repo to djformby's original

--- hmi_vm/hmi-build.sh
+++ hmi_vm/hmi-build.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get -y install vim-gtk locate lsof subversion wine python-wxgtk3.0 pyro python-numpy python-nevow python-matplotlib python-lxml python-zeroconf
 svn export https://github.com/thiagoralves/OpenPLC_Files/trunk/Software/PLCopen%20Editor%20v1.3%20-%20Linux.zip
 unzip 'PLCopen Editor 1.3 - Linux.zip'
-svn export https://github.com/Valiant-Mouse/GRFICS/trunk/plc_vm
+svn export https://github.com/djformby/GRFICS/trunk/plc_vm
 svn export https://github.com/djformby/GRFICS/trunk/hmi_vm
 cd hmi_vm/HMI
 wine AdvancedHMI.exe
