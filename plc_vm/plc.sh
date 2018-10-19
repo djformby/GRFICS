a58d526d534204f1d78cec1251b3622fd8c802a0
Valiant-Mouse <31675444+Valiant-Mouse@users.noreply.github.com>
2018-10-19 14:09:34 -0400

    Update plc.sh

--- plc_vm/plc.sh
+++ plc_vm/plc.sh
@@ -1,10 +1,16 @@
-            #!/bin/bash
-            apt-get -y update
-            apt-get -y upgrade
-            apt-get -y install locate dnsutils lsof
-            apt-get -y install vim-gtk subversion build-essential pkg-config bison flex autoconf automake libtool make nodejs git
-            svn export https://github.com/Valiant-Mouse/GRFICS/trunk/plc_vm
-            mv plc_vm /home/user
-            cd /home/user/plc_vm/OpenPLC_v2-master
-            sudo bash ./build-noninteractive-MODBUS-only.sh
-            sudo nodejs server.js
+#!/bin/bash
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install locate dnsutils lsof
+apt-get -y install vim-gtk subversion build-essential pkg-config bison flex autoconf automake libtool make nodejs git
+svn export https://github.com/Valiant-Mouse/GRFICS/trunk/plc_vm
+pwd | grep '/home/' &> /dev/null
+if [ $? == 1 ]; then
+  ls |grep 'plc_vm' &> /dev/null
+  if [ $? == 0 ]; then
+    mv plc_vm ~
+  fi
+fi
+cd ~/plc_vm/OpenPLC_v2-master
+sudo bash ./build-noninteractive-MODBUS-only.sh
+sudo nodejs server.js
