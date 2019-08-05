5b23592823b915f250c575fb4aa947c2980e9f29
Valiant-Mouse <31675444+Valiant-Mouse@users.noreply.github.com>
2019-08-05 15:25:57 -0400

    Update tank.py

--- simulation_vm/simulation/remote_io/tank.py
+++ simulation_vm/simulation/remote_io/tank.py
@@ -40,6 +40,13 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 # --------------------------------------------------------------------------- #
+# define network
+# --------------------------------------------------------------------------- #
+
+tank_ip_address = "192.168.95.14"
+modbusTCP_port = 502
+
+# --------------------------------------------------------------------------- #
 # define your callback process
 # --------------------------------------------------------------------------- #
 
@@ -105,7 +112,7 @@ def run_update_server():
     time = 1  # 5 seconds delay
     loop = LoopingCall(f=updating_writer, a=(context,sock))
     loop.start(time, now=False)  # initially delay by time
-    StartTcpServer(context, identity=identity, address=("192.168.95.14", 502))
+    StartTcpServer(context, identity=identity, address=(tank_ip_address, modbusTCP_port))
 
 
 if __name__ == "__main__":
